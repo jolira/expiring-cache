7ee2e7767010c37d2c993b612cdf28f091ae4a7c
Joachim Kainz <jfk@jolira.com>
2011-08-26 13:02:44 -0700

    making the CacheEntry public as per user request.

--- src/main/java/com/google/code/joliratools/cache/CacheEntry.java
+++ src/main/java/com/google/code/joliratools/cache/CacheEntry.java
@@ -13,13 +13,30 @@ package com.google.code.joliratools.cache;
 import java.io.Serializable;
 import java.lang.ref.SoftReference;
 
-class CacheEntry<V> implements Serializable {
+/**
+ * The entry used to store the entry as well as the time the entry expires. This
+ * class has become public in version 1.0.16 of this library as users would like
+ * to inherit from it. My strong recommendation would be not to inherit from this
+ * class but use aggregation instead.
+ * 
+ * @author jfk
+ * @since 1.0
+ *
+ * @param <V>
+ */
+public class CacheEntry<V> implements Serializable {
     private static final long serialVersionUID = -4063049639315804932L;
 
     final transient SoftReference<V> ref;
     final transient long expiration;
 
-    CacheEntry(final V value, final long expiration) {
+    /**
+     * Create a new entry.
+     * 
+     * @param value the value
+     * @param expiration the time the value expires
+     */
+    public CacheEntry(final V value, final long expiration) {
         ref = new SoftReference<V>(value);
         this.expiration = expiration;
     }
